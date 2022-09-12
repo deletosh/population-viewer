7d8148aa07623e3bfeeaca75e962cbaff6144bfa
Dele Omotosho <dele@authoritywit.com>
2022-09-12 11:51:17 -0400

    shows all matching states

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
-const USADATA_API_URL = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest';
+const USADATA_API_URL = 'https://datausa.io/api/data?drilldowns=State&measures=Population'
+    //'&year=latest';
 // ?drilldowns=Nation&measures=Population&year=latest
 
 const searchInput = document.querySelector('#find-state');
 const showStateEl = document.querySelector('#show-state');
 
-// Testing the API that it returns the data we expect....
-// fetch(USADATA_API_URL)
-//     .then(res => res.json())
-//     .then(ourData => {
-//         console.log(ourData);
-//         console.log(ourData.data);
-//         console.log(ourData.source);
-//     })
-//     .catch(err => console.error(err));
-
-// [[ Alaska ]]
 searchInput.addEventListener('blur', async (event) => {
-    // console.log(event.target.value);
-    // console.log(searchInput.value);
-
-    // we should get what the user types
-    const state = await getStateData(event.target.value, '2020'); // { }
-
-
+    const states = await getStateData(event.target.value, '2020');
     showStateEl.innerHTML = ''
-    // console.log(state);// {}
-    // console.log(state);
-    showStateData(state);
-});
 
-// const myData = await fetch('')
-// fetch(USADATA_API_URL)
-//     .then(res => res.json())
-//     .then(ourData => {
-//         console.log(ourData);
-//         console.log(ourData.data);
-//         console.log(ourData.source);
-//     })
-//     .catch(err => console.error(err));
+    // 1. v0.75 = state: {}
+    // showStateData(state);
+
+    // 2. state: []
+    // console.log(state);
+    states.forEach( aState => showStateData(aState) )
+});
 
 // async / await
 async function getStateData(stateName, year) {
@@ -46,12 +24,9 @@ async function getStateData(stateName, year) {
     const stateObj = await response.json();
 
     const foundState = stateObj.data.filter(item => item.State === stateName);
-    // return all the statistucs that matches Alaska
-    // console.log(stateObj.data);
-    // API????
 
-    // console.log(foundState[0]);
-    return foundState[0];
+
+    return foundState;
 }
 
 function showStateData(stateName) {
